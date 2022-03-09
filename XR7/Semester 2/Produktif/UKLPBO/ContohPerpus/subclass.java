1d76ff16f8c6dac08ce8add742ae7b6a2b1abf9b
Nay45 <101272430+Nay45@users.noreply.github.com>
2022-03-09 23:00:28 +0700

    Delete XR7/Semester 2/Produktif/UKLPBO directory

--- XR7/Semester 2/Produktif/UKLPBO/ContohPerpus/subclass.java
+++
@@ -1,7 +0,0 @@
-package UKLPBO.ContohPerpus;
-
-public class subclass extends superclass{
-    subclass(){
-        super("pesan");
-    }
-}
--- XR7/Semester 2/Produktif/UKLPBO/ContohPerpus/superclass.java
+++
@@ -1,8 +0,0 @@
-package UKLPBO.ContohPerpus;
-
-public class superclass {
-    String pesan = "Pesan Default ini Ditambah : ";
-    superclass(String param){
-        System.out.println(this.pesan + param);
-    }
-}
