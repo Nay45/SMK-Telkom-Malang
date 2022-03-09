b3018eaa0693c858cd76e459e13f18e35c7f9c7a
Nay45 <101272430+Nay45@users.noreply.github.com>
2022-03-09 22:36:33 +0700

    Add files via upload

---
+++ XR7/METIC/src/metic/Pertemuan3Array.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metic;
+
+/**
+ *
+ * @author USER
+ */
+public class Pertemuan3Array {
+    public static void main(String[] args){
+        int[]array = {1,2,3,4,5,6,7,8,9,10};
+        
+        for(int angka:array){
+            System.out.println(angka);
+        }
+    }
+//    catch (Exception e){
+//            System.out.println("ada error "+ e);
+//}
+//    static void noArguMethod(){
+//            System.out.println("print dari biasa");
+//    }
+//    public static void main(String[] args) {
+//        printAll();
+//    }
+}
